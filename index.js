7b5d3074ee58e144222525f69770a73e91b631da
Kentaro Yamauchi <YAMAUCHI-Kentarou@tdi.co.jp>
2018-05-25 17:56:28 +0900

    ajaxテスト用jsを追記

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 $(function(){
 	$("#content").append("js test.");
+	
+	$.ajax({
+	url: 'http://www.ekidata.jp/api/p/20.json',
+	type:'GET',
+	dataType: 'script',
+	timeout:1000,
+	success: function(data, dataType) {
+		alert("ok");
+		console.log("ok", xml.data);
+	},
+	error: function(XMLHttpRequest, textStatus, errorThrown) {
+		alert("ng");
+		console.log("ng", XMLHttpRequest, textStatus, errorThrown);
+	}
+});
 });
