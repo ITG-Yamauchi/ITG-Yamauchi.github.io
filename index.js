af5a29714cee7f19f6ed2d0383ec7b84f0adc65d
Kentaro Yamauchi <YAMAUCHI-Kentarou@tdi.co.jp>
2018-05-25 18:05:11 +0900

    ajax通信テスト用jsを修正

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 $(function(){
-	$("#content").append("js test.");
+	// js動くかテスト
+	var content = $("#content")
+	content.append("js test.");
 	
+	// ajaxできるかテスト
 	$.ajax({
 	url: 'https://www.ekidata.jp/api/p/20.json',
 	type:'GET',
@@ -8,7 +11,13 @@ $(function(){
 	timeout:1000,
 	success: function(data, dataType) {
 		alert("ok");
-		console.log("ok", xml.data);
+		
+		var lineData = xml.data["line"];
+		console.log("ok", lineData);
+		
+		for (var i = 0; i < lineData.length; i++) {
+			content.append("<div>" + lineData[i]["line_name"] + "</div>")
+		}
 	},
 	error: function(XMLHttpRequest, textStatus, errorThrown) {
 		alert("ng");
